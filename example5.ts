695fa7b820965dbae9348221503473493dd2f420
terria1020 <jaehan1346@gmail.com>
2024-07-16 23:28:20 +0900

    update: void 타입과 never 타입에 대한 예시 추가

--- example5.ts
+++ example5.ts
@@ -10,3 +10,19 @@ if (typeof a === 'number') {
     let d = a + b // a의 타입을 확정하고 있기 때문에 최소한 이 블럭 내에서는 오류 X
 }
 // 타입을 잘은 모르지만 강타입으로 만든다고 보면 될듯
+
+
+// 아무것도 반환하지 않을 함수: void 타입
+function foo() {
+    console.log("hello world!")
+}
+
+// void 타입을 명시할 수도 있음
+function foo2() : void {
+    console.log("hello world!")
+}
+
+// "절대 반환하지 않을 함수" 명시: never 
+function foo3() : never {
+    throw new Error("error")
+}
