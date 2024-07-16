5428c7bf1adc220c7fcdfc669a70880b40155d04
terria1020 <jaehan1346@gmail.com>
2024-07-16 22:42:02 +0900

    update: 오류 케이스 추가

--- example2.ts
+++ example2.ts
@@ -8,3 +8,12 @@ const player: {
     age: 12
 }
 // name과 age 를 가지는 객체인 player를 생성했음. 근데 optional 하게 할 수 있나 ?
+
+const player2: {
+    name: string,
+    age: number
+} = {
+    name: "안녕하세요"
+}
+
+// 오류가 남
