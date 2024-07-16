679641e95c3605e51d8293d73f20262b35d7d47e
terria1020 <jaehan1346@gmail.com>
2024-07-16 22:48:09 +0900

    update: 타입 선언 추가

--- example2.ts
+++ example2.ts
@@ -1,27 +1,24 @@
 
-// player 객체가 있는데, name은 무조건 가지지만 age는 일부는 가지고 있고 일부는 가지고 있지 않는다 ?
-const player: {
+type Player = {
     name: string,
-    age: number
-} = {
+    age?: number
+}
+
+
+// player 객체가 있는데, name은 무조건 가지지만 age는 일부는 가지고 있고 일부는 가지고 있지 않는다 ?
+const player: Player = {
     name: "안녕하세요",
     age: 12
 }
 // name과 age 를 가지는 객체인 player를 생성했음. 근데 optional 하게 할 수 있나 ?
 
-const player2: {
-    name: string,
-    age: number
-} = {
+const player2: Player = {
     name: "안녕하세요"
 }
 
 // 오류가 남
 
-const player3: {
-    name: string,
-    age?: number
-} = {
+const player3: Player = {
     name: "안녕하세요"
 }
 // 물음표 기호 추가 시 타입을 age?: number | undefined 라고 표기하고 있고, 오류가 사라짐
