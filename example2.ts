7ca80a5ab8cca41e35b4e33a352fca5a444e468e
terria1020 <jaehan1346@gmail.com>
2024-07-16 22:41:31 +0900

    create: example2.ts 파일 추가

---
+++ example2.ts
@@ -0,0 +1,10 @@
+
+// player 객체가 있는데, name은 무조건 가지지만 age는 일부는 가지고 있고 일부는 가지고 있지 않는다 ?
+const player: {
+    name: string,
+    age: number
+} = {
+    name: "안녕하세요",
+    age: 12
+}
+// name과 age 를 가지는 객체인 player를 생성했음. 근데 optional 하게 할 수 있나 ?
