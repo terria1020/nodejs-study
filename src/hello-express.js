c7695d3f1459b642014babf2e922ee2be99db6b4
terria1020 <jaehan1346@gmail.com>
2024-07-18 05:23:35 +0900

    update: path 파라메터를 읽어서 응답하는 예시 API 추가

--- src/hello-express.js
+++ src/hello-express.js
@@ -1,4 +1,5 @@
 
+const { equal } = require('assert');
 const express = require('express')
 
 const app = express()
@@ -15,6 +16,14 @@ app.get("/", (req, res) => {
     })
     // res.end("{\"hello\": \"world\"}")
 });
+
+app.get("/foo/:id", (req, res) => {
+    const id = req.params.id
+
+    res.json({
+        "message": `hello, this id is: ${id}`
+    })
+})
 
 app.listen(port, () => {
     console.log(`server listen on ${port}...`)
