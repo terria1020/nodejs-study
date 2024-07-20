4d99cf02052d30f474d6282fa5208ae86cda5ff0
terria1020 <jaehan1346@gmail.com>
2024-07-20 13:11:11 +0900

    docs: 사용자 관련 API의 설명 추가

--- src/user/v1/dto/res/resAllUsers.dto.ts
+++ src/user/v1/dto/res/resAllUsers.dto.ts
@@ -1,3 +1,5 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ResAllUsersDto {
   constructor(userId: number, name: string, email: string) {
     this.userId = userId;
@@ -5,7 +7,12 @@ export class ResAllUsersDto {
     this.email = email;
   }
 
+  @ApiProperty({ description: '사용자 id' })
   userId: number;
+
+  @ApiProperty({ description: '사용자 이름' })
   name: string;
+
+  @ApiProperty({ description: '사용자 이메일' })
   email: string;
 }
--- src/user/v1/user.controller.ts
+++ src/user/v1/user.controller.ts
@@ -1,24 +1,45 @@
 import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { ReqRegisterDto } from './dto/req/reqRegister.dto';
 import { UserService } from './user.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ResAllUsersDto } from './dto/res/resAllUsers.dto';
 
 @Controller({
   path: 'api/v1/users',
 })
+@ApiTags('User API')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('')
+  @ApiOperation({
+    summary: '사용자 전체 조회',
+    description: '사용자 전체 조회 API',
+  })
+  @ApiResponse({
+    type: [ResAllUsersDto],
+  })
   getAll() {
     return this.userService.getAll();
   }
 
   @Get(':userId')
+  @ApiOperation({
+    summary: '특정 사용자 조회',
+    description: '특정 사용자 조회 API',
+  })
+  @ApiResponse({
+    type: ResAllUsersDto,
+  })
   getUser(@Param('userId') userId: number) {
     return this.userService.getUser(userId);
   }
 
   @Post('/register')
+  @ApiOperation({
+    summary: '회원가입',
+    description: '회원가입 API',
+  })
   userRegister(@Body() dto: ReqRegisterDto) {
     Logger.log('POST register');
     return this.userService.register(dto);
