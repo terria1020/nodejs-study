d9c9325813461cf2730fbded6880c660ebe2f5f0
terria1020 <jaehan1346@gmail.com>
2025-01-21 03:37:05 +0900

    update: app.ts의 app 객체 변경, 기본 라우트 추가, app export 추가

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import * as express from "express";
 
-const app: express.Application = express.application;
-
+const app: express.Express = express();
 const port = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.status(200).send("Hello World");
+});
 
 app.listen(port, () => {
     console.log(`server listen on ${port}...`);
 });
+
+export default app;
