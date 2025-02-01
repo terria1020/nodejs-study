028a0cf191c807807678ac5562745e814f5ab3e9
terria1020 <jaehan1346@gmail.com>
2025-02-01 09:14:46 +0900

    update: configmodule 전역 설정 추가

--- cat-community/src/app.module.ts
+++ cat-community/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongoModule } from './databases/mongo/mongo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.development', '.env.production'],
+    }),
+    MongoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
