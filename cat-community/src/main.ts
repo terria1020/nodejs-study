c131f04ad616c752fe62980422f48ab1eaccd885
terria1020 <jaehan1346@gmail.com>
2025-02-02 14:29:52 +0900

    update: nest 앱 기본 설정 추가, cors 설정 추가

--- cat-community/src/main.ts
+++ cat-community/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const allowedOrigins: string[] = configService
+    .get('ORIGIN_URL')
+    .split(',') || ['http://localhost:3000'];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(configService.get('SERVER_PORT'));
 }
 bootstrap();
